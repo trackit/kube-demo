32b7fb85c542db1c27d5bbeb7fcb135eed3b8eab
Arthur Knoepflin <arthur@knoepflin.eu>
2022-03-21 18:09:33 +0100

    feat(metrics): add prometheus + hpa

--- app/index.ts
+++ app/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { router as ping } from './ping';
 import { router as fs } from './fs';
 import { router as auth } from './auth';
+import { router as metrics } from './metrics';
 
 const app = express();
 
 app.use('/ping', ping);
 app.use('/fs', fs);
 app.use('/auth', auth);
+app.use('/metrics', metrics);
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`App started on port ${process.env.PORT || 3000}`);
